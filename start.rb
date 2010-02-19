44bb0661659ca192707d84b3b4379f24e857ddd2
Mike Green <mike.is.green@gmail.com>
2010-02-19 15:31:01 -0500

    set test signed-in values

--- start.rb
+++ start.rb
@@ -19,25 +19,27 @@ end
 
 post "/tag" do
 	if params[:auth_key] == AUTH_KEY
-		tag = params[:tag].chomp
-		user = DB[:users].filter(:rfid_tag => tag)
+		if params[:tag].size > 9
+			tag = params[:tag].chomp
+			user = DB[:users].filter(:rfid_tag => tag)
 		
-		if user.count > 0
-			user_data = user.first
-			LOGGER.info("RFID card swiped by user. Tag ID: #{tag}, User: #{user_data[:name]}.")
-			new_scan_count = user_data[:scan_count] + 1
-			user.update(:scan_count => new_scan_count, :last_scanned_at => Time.now)
+			if user.count > 0
+				user_data = user.first
+				LOGGER.info("RFID card swiped by user. Tag ID: #{tag}, User: #{user_data[:name]}.")
+				new_scan_count = user_data[:scan_count] + 1
+				user.update(:scan_count => new_scan_count, :last_scanned_at => Time.now)
 			
-			if user_data[:signed_in]
-				time_difference = Time.now - user_data[:last_scanned_at]
-				voice = "Goodbye, #{user_data[:name]}. You worked for #{time_difference.to_i} seconds. Thanks for working at Converge."
-				user.update(:signed_in => false)
-			else
-				voice = `say "Hello #{user_data[:name]}. Welcome to Converge."`
-				user.update(:signed_in => true)
+				if user_data[:signed_in]
+					time_difference = Time.now - user_data[:last_scanned_at]
+					voice = `say --voice=Vicki "Goodbye, #{user_data[:name]}. You worked for #{time_difference.to_i} seconds. Thanks for working at Converge."`
+					user.update(:signed_in => false)
+				else
+					voice = `say --voice=Vicki "Hello #{user_data[:name]}. Welcome to Converge."`
+					user.update(:signed_in => true)
+				end
+			
+				"OK"
 			end
-			
-			"OK"
 		else
 			LOGGER.info("User not found for card ID #{tag}")
 			"ER"
