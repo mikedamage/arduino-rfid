594cbded906f456aa5b623d9684903eb2ea0de3b
Mike Green <mike.is.green@gmail.com>
2010-02-19 11:03:06 -0500

    inserted some sample data into sqlite database

--- start.rb
+++ start.rb
@@ -1,3 +1,4 @@
+require "logger"
 require "rubygems"
 require "sequel"
 require "digest/sha1"
@@ -6,8 +7,12 @@ require "sinatra"
 configure do
 	DB = Sequel.sqlite("db/development.sqlite3")
 	AUTH_KEY = Digest::SHA1.hexdigest("fifthroom")[0..10]
+	LOGGER = Logger.new("log/development.log")
+	LOGGER.level = Logger::INFO
 end
 
 post "/noise" do
-	
+	if params[:auth_key] == AUTH_KEY
+		
+	end
 end
