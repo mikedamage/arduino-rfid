2e5c3b013ac6b90d2cae482897185bafd68f2cd3
Mike Green <mike.is.green@gmail.com>
2010-02-19 11:46:39 -0500

    basic data read/write plus text to speech on card swipe

--- start.rb
+++ start.rb
@@ -11,8 +11,27 @@ configure do
 	LOGGER.level = Logger::INFO
 end
 
-post "/noise" do
+post "/noise_error" do
 	if params[:auth_key] == AUTH_KEY
+		LOGGER.info("Received random radio noise.")
+	end
+end
+
+post "/tag" do
+	if params[:auth_key] == AUTH_KEY
+		tag = params[:tag]
+		user = DB[:users].filter(:rfid_tag => tag)
 		
+		if user.count > 0
+			user_data = user.first
+			LOGGER.info("RFID card swiped by user. Tag ID: #{tag}, User: #{user_data[:name]}.")
+			new_scan_count = user_data[:scan_count] + 1
+			user.update(:scan_count => new_scan_count, :last_scanned_at => Time.now)
+			voice = `say "Hello #{user_data[:name]}. Welcome to Converge."`
+			"OK"
+		else
+			LOGGER.info("User not found for card ID #{tag}")
+			"ER"
+		end
 	end
 end
